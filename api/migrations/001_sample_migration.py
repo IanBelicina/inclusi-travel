fe819ad7a4d5ad7e552058ddd63792c7ddbe8b00
Ian <vancebelicina@gmail.com>
2023-08-14 18:54:08 -0700

    attempting migrations

--- api/migrations/001_sample_migration.py
+++ api/migrations/001_sample_migration.py
@@ -2,37 +2,104 @@ steps = [
     [
         # "Up" SQL statement
         """
-        CREATE TABLE dummy (
-            id SERIAL PRIMARY KEY NOT NULL,
-            required_limited_text VARCHAR(1000) NOT NULL,
-            required_unlimited_text TEXT NOT NULL,
-            required_date_time TIMESTAMP NOT NULL,
-            automatically_set_date_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            required_integer INTEGER NOT NULL,
-            required_money MONEY NOT NULL
+        CREATE TABLE accounts (
+            id SERIAL NOT NULL UNIQUE,
+            first_name VARCHAR(250) NOT NULL,
+            last_name VARCHAR(250) NOT NULL,
+            date_of_birth DATE NOT NULL,
+            email VARCHAR(250) NOT NULL,
+            username VARCHAR(250) NOT NULL UNIQUE,
+            password VARCHAR(250) NOT NULL
+
         );
+
+
         """,
         # "Down" SQL statement
         """
-        DROP TABLE dummy;
+        DROP TABLE accounts;
         """
     ],
     [
         # "Up" SQL statement
         """
-        CREATE TABLE big_dummy (
-            id SERIAL PRIMARY KEY NOT NULL,
-            required_limited_text VARCHAR(1000) NOT NULL,
-            required_unlimited_text TEXT NOT NULL,
-            required_date_time TIMESTAMP NOT NULL,
-            automatically_set_date_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            required_integer INTEGER NOT NULL,
-            required_money MONEY NOT NULL
+        CREATE TABLE accessibilities (
+            id SERIAL NOT NULL UNIQUE,
+            name VARCHAR(250) NOT NULL
+
         );
         """,
         # "Down" SQL statement
         """
-        DROP TABLE big_dummy;
+        DROP TABLE accessibilities;
         """
-    ]
+    ],
+    [
+        # "Up" SQL statement
+        """
+        CREATE TABLE locations (
+            id SERIAL NOT NULL UNIQUE,
+            address VARCHAR(250) NOT NULL,
+            city VARCHAR(250) NOT NULL,
+            state VARCHAR(250) NOT NULL,
+            location_name VARCHAR(250) NOT NULL,
+            picture TEXT,
+            updated_on DATE NOT NULL
+        );
+        """,
+        # "Down" SQL statement
+        """
+        DROP TABLE locations;
+        """
+    ],
+    [
+        # "Up" SQL statement
+        """
+        CREATE TABLE comments (
+            id SERIAL NOT NULL UNIQUE,
+            account_id INTEGER NOT NULL REFERENCES accounts("id") ON DELETE CASCADE,
+            review_id INTEGER NOT NULL REFERENCES reviews("id") ON DELETE CASCADE,
+            content TEXT NOT NULL,
+            created_on TIMESTAMP NOT NULL
+
+        );
+        """,
+        # "Down" SQL statement
+        """
+        DROP TABLE comments;
+        """
+    ],
+    [
+        # "Up" SQL statement
+        """
+        CREATE TABLE reviews (
+            id SERIAL NOT NULL UNIQUE,
+            location_id NOT NULL INTEGER REFERENCES locations("id") ON DELETE CASCADE,
+            account_id NOT NULL INTEGER REFERENCES accounts("id") ON DELETE CASCADE, rating INTEGER NOT NULL check(rating = 1 or rating = 2 or rating = 3 or rating = 4 or rating = 5),
+            body TEXT NOT NULL,
+            created_on TIMESTAMP NOT NULL
+        );
+        """,
+        # "Down" SQL statement
+        """
+        DROP TABLE reviews;
+        """
+    ],
+    [
+        # "Up" SQL statement
+        """
+        CREATE TABLE location_accessibilities (
+            location_id INTEGER NOT NULL REFERENCES locations("id") ON DELETE CASCADE,
+            acessibility_id INTEGER NOT NULL REFERENCES accessibilities("id") ON DELETE CASCADE
+
+        );
+
+        """,
+        # "Down" SQL statement
+        """
+        DROP TABLE location_accessibilities;
+        """
+    ],
+
+
 ]
