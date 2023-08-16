10572a5233f7ba41934229d73f6bd1a7b6945c0e
Ian <vancebelicina@gmail.com>
2023-08-16 15:14:29 -0700

    now able to get all comments for specific review with reviewOut pydantic model included. Next going to add to function but implement accountout pydantic model

--- api/queries/comments.py
+++ api/queries/comments.py
@@ -1,3 +1,4 @@
+from queries.reviews import ReviewOut
 from pydantic import BaseModel
 from typing import Optional, List, Union
 from datetime import date
@@ -17,6 +18,123 @@ class CommentIn(BaseModel):
 class CommentOut(BaseModel):
     id: int
     account_id: int
-    review_id: int
     content: str
     created_on: date
+    review: ReviewOut
+
+
+class CommentRepository:
+
+    def get_all_review_comments(self, review_id:int) -> Union[List[CommentOut], Error]:
+
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    db.execute(
+                        """
+                        SELECT
+
+                        c.id as comment_id
+                        ,c.account_id as account_id_comment
+                        ,c.content as content
+                        ,c.created_on as created_on_comment
+                        ,r.id as review_id
+                        ,r.location_id as location_id
+                        ,r.account_id as account_id_review
+                        ,r.rating as rating
+                        ,r.body as body
+                        ,r.created_on as created_on_review
+                        from comments c
+                        join reviews r on c.review_id = r.id
+                        where review_id =  %s
+                        """,
+                        [review_id]
+                    )
+                    comments = []
+                    rows = db.fetchall()
+                    # print(rows)
+                    for row in rows:
+                        comment = self.comment_record_to_dict(row, db.description)
+                        # print(comment)
+                        comments.append(comment)
+                    return comments
+
+        except Exception as e:
+            print(e)
+            return {"message":"Could not get all comments"}
+
+
+##################################### FOR STRETCH GOAL FEATURING ACTIVITY? ##################
+    # def get_all_user_comments(self, account_id:int) -> Union[List[CommentOut], Error]:
+
+    #         try:
+    #             with pool.connection() as conn:
+    #                 with conn.cursor() as db:
+    #                     db.execute(
+    #                         """
+    #                         SELECT
+    #                             id
+    #                             ,account_id
+    #                             ,review_id
+    #                             ,content
+    #                             ,created_on
+    #                         FROM comments
+    #                         WHERE account_id = %s
+    #                         """,
+    #                         [account_id]
+    #                     )
+    #                     result = []
+    #                     for record in db:
+    #                         comment = CommentOut(
+    #                             id=record[0],
+    #                             account_id=record[1],
+    #                             review_id=record[2],
+    #                             content=record[3],
+    #                             created_on=record[4]
+    #                         )
+    #                         result.append(comment)
+    #                     return result
+    #         except Exception as e:
+    #             print(e)
+    #             return {"message":"Could not get all comments"}
+###############################################################################################################################################
+    def comment_record_to_dict(self, row, description) -> CommentOut:
+
+        comment = None
+        if row is not None:
+            comment = {}
+            comment_fields = [
+                "comment_id",
+                "account_id_comment",
+                # "review_id_comment",
+                "content",
+                "created_on_comment"
+            ]
+            for i, column in enumerate(description):
+                if column.name in comment_fields:
+                    comment[column.name] = row[i]
+            comment["id"] = comment["comment_id"]
+            comment["account_id"] = comment["account_id_comment"]
+            comment["created_on"] = comment["created_on_comment"]
+
+            review = {}
+            review_fields = [
+                "review_id",
+                "location_id",
+                "account_id_review",
+                "rating",
+                "body",
+                "created_on_review"
+            ]
+            for i, column in enumerate(description):
+                if column.name in review_fields:
+                    review[column.name] = row[i]
+            review["id"] = review["review_id"]
+            review["account_id"] = review["account_id_review"]
+            review["created_on"] = review["created_on_review"]
+
+            comment["review"] = review
+
+            print(comment)
+
+            return CommentOut(**comment)
--- api/queries/reviews.py
+++ api/queries/reviews.py
@@ -1,25 +1,26 @@
 from pydantic import BaseModel
 
 from queries.locations import LocationsOut
-
+from datetime import date
 
 class ReviewIn(BaseModel):
-    
+
     location_id: int
     account_id: int
     rating: int
     body: str
-    created_on: str
+    created_on: date
 
 
 
 class ReviewOut(BaseModel):
     id: int
-    location_id: LocationsOut
-    account_id: AccountOut
+    # location_id: LocationsOut
+    location_id:int
+    account_id: int
     rating: int
     body: str
-    created_on: str
+    created_on: date
 
 
 
@@ -29,11 +30,9 @@ class ReviewOut(BaseModel):
 #         CREATE TABLE reviews (
 #             id SERIAL NOT NULL UNIQUE,
 #             location_id INTEGER NOT NULL REFERENCES locations("id") ON DELETE CASCADE,
-#             account_id INTEGER NOT NULL REFERENCES accounts("id") ON DELETE CASCADE, 
+#             account_id INTEGER NOT NULL REFERENCES accounts("id") ON DELETE CASCADE,
 #              rating INTEGER NOT NULL check(rating = 1 or rating = 2 or rating = 3 or rating = 4 or rating = 5),
 #             body TEXT NOT NULL,
 #             created_on TIMESTAMP NOT NULL
 #         );
 #         """,
-
-
