ce9a1a945d5d383071ec5efd6a6ae96f4f0c0783
Drew Norman-Meadows <norma1dj@gmail.com>
2023-08-16 17:19:04 -0700

    tshooting get review

--- api/queries/reviews.py
+++ api/queries/reviews.py
@@ -5,19 +5,18 @@ from queries.locations import LocationsOut
 from queries.accounts import AccountOut
 from datetime import date
 
-class ReviewOut(BaseModel):
-    id: int
-    location_id: LocationsOut
+class ReviewIn(BaseModel):
+
+    location_id: int
     account_id: int
     rating: int
     body: str
     created_on: date
 
-
-class ReviewIn(BaseModel):
-
-    location_id: int
-    account_id: int
+class ReviewOut(BaseModel):
+    id: int
+    location_id: LocationsOut
+    account_id: AccountOut
     rating: int
     body: str
     created_on: date
@@ -27,15 +26,47 @@ class ReviewListOut(BaseModel):
 
 
 class ReviewQueries:
+    def create_review(self, review: ReviewIn) -> ReviewOut:
+        with pool.getconn() as conn:
+            with conn.cursor() as cur:
+                cur.execute(
+                    """
+                    INSERT INTO reviews (location_id, account_id, rating, body, created_on)
+                    VALUES (%s, %s, %s, %s, %s)
+                    RETURNING id, location_id, account_id, rating, body, created_on
+                    """,
+                    (review.location_id, review.account_id, review.rating, review.body, review.created_on),
+                )
+                
+                    
+                row = cur.fetchone()
+
+                # Initialize an empty dictionary to store the column name-value pairs
+                row_dict = {}
+
+                # Loop through the columns and values in the fetched row
+                for i, column in enumerate(cur.description):
+                    column_name = column.name  # Get the column name
+                    column_value = row[i]      # Get the corresponding value from the row
+                    row_dict[column_name] = column_value  # Add the column name-value pair to the dictionary
+
+                # Create a ReviewOut object using the dictionary values
+                review_out_object = ReviewOut(**row_dict)
+
+                # Return the newly created ReviewOut object
+                return review_out_object
+
     def get_all_reviews(self) -> List[ReviewOut]:
             with pool.connection() as conn:
                 with conn.cursor() as cur:
                     cur.execute(
                         """
                         SELECT r.id, r.location_id, r.account_id, r.rating,
-                        r.body, r.created_on, l.location_name, l.address, l.city, l.state, l.updated_on, l.picture
+                        r.body, r.created_on, l.location_name, l.address, l.city, l.state, l.updated_on, l.picture,
+                        a.first_name, a.last_name, a.date_of_birth, a.email, a.username, a.password
                         FROM reviews r
                         JOIN locations l ON r.location_id = l.id
+                        JOIN accounts a ON r.account_id = a.id
                         ORDER BY r.created_on;
                         """
                     )
@@ -54,11 +85,21 @@ class ReviewQueries:
                             state=record['state'],
                             location_name=record['location_name'],
                             updated_on=record['updated_on'],
-                            picture=record['picture'] or '', # if its empty
+                            picture=record['picture'] or '', # if it's empty
                         )
 
+                        account = AccountOut(
+                            id=record['account_id'],
+                            first_name=record['first_name'],
+                            last_name=record['last_name'],
+                            date_of_birth=record['date_of_birth'],
+                            email=record['email'],
+                            username=record['username'],
+                            password=record['password']
+                        )
                         
                         record['location_id'] = location
+                        record['account_id'] = account
 
                         results.append(ReviewOut(**record))
 
