defcacf0cffefbf454a334fd46360975173a530d
Drew Norman-Meadows <norma1dj@gmail.com>
2023-08-25 15:22:17 -0700

    make a better review form

--- ghi/src/CreateReview.js
+++ ghi/src/CreateReview.js
@@ -2,11 +2,13 @@ import { useContext } from "react";
 import { AuthContext } from "@galvanize-inc/jwtdown-for-react";
 import React, { useState } from "react";
 
+
 const CreateReview = () => {
   const [locationId, setLocationId] = useState("");
   const [accountId, setAccountId] = useState("");
   const [rating, setRating] = useState("");
   const [body, setBody] = useState("");
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
   const { token }  = useContext(AuthContext); 
 
@@ -36,6 +38,7 @@ const CreateReview = () => {
       } else {
         const data = await response.json();
         console.log(data);
+        setIsSubmitted(true);
       }
     } catch (error) {
       console.error("There was an error!", error);
@@ -43,65 +46,71 @@ const CreateReview = () => {
   };
 
   return (
-    //
-    <div className="container mt-5">
-      <form onSubmit={handleSubmit}>
-        <div className="mb-3">
-          <label htmlFor="locationId" className="form-label">
-            Location ID:
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            id="locationId"
-            value={locationId}
-            onChange={(e) => setLocationId(e.target.value)}
-          />
-        </div>
+    <div className="card text-bg-light mb-3">
+      <h5 className="card-header">New Review</h5>
+      <div className="card-body">
+        {isSubmitted ? (
+          <p>Thanks for your review!</p>
+        ) : (
+          <form onSubmit={handleSubmit}>
+            <div className="mb-3">
+              <label htmlFor="locationId" className="form-label">
+                Location ID:
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="locationId"
+                value={locationId}
+                onChange={(e) => setLocationId(e.target.value)}
+              />
+            </div>
 
-        <div className="mb-3">
-          <label htmlFor="accountId" className="form-label">
-            Account ID:
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            id="accountId"
-            value={accountId}
-            onChange={(e) => setAccountId(e.target.value)}
-          />
-        </div>
+            <div className="mb-3">
+              <label htmlFor="accountId" className="form-label">
+                Account ID:
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="accountId"
+                value={accountId}
+                onChange={(e) => setAccountId(e.target.value)}
+              />
+            </div>
 
-        <div className="mb-3">
-          <label htmlFor="rating" className="form-label">
-            Rating:
-          </label>
-          <input
-            type="number" // changed to number for better semantics
-            className="form-control"
-            id="rating"
-            value={rating}
-            onChange={(e) => setRating(e.target.value)}
-          />
-        </div>
+            <div className="mb-3">
+              <label htmlFor="rating" className="form-label">
+                Rating:
+              </label>
+              <input
+                type="number" // changed to number for better semantics
+                className="form-control"
+                id="rating"
+                value={rating}
+                onChange={(e) => setRating(e.target.value)}
+              />
+            </div>
 
-        <div className="mb-3">
-          <label htmlFor="body" className="form-label">
-            Body:
-          </label>
-          <textarea
-            className="form-control"
-            id="body"
-            rows="3" // for visual presentation
-            value={body}
-            onChange={(e) => setBody(e.target.value)}
-          />
-        </div>
+            <div className="mb-3">
+              <label htmlFor="body" className="form-label">
+                Body:
+              </label>
+              <textarea
+                className="form-control"
+                id="body"
+                rows="3" // for visual presentation
+                value={body}
+                onChange={(e) => setBody(e.target.value)}
+              />
+            </div>
 
-        <button type="submit" className="btn btn-primary">
-          Submit
-        </button>
-      </form>
+            <button type="submit" className="btn btn-primary">
+              Submit Review
+            </button>
+          </form>
+        )}
+      </div>
     </div>
   );
 };
