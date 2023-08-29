dda7813157dc4b2d8ed2b44539c3b24ba9fe1703
Drew Norman-meadows <norma1dj@gmail.com>
2023-08-29 22:38:44 +0000

    Homepage/Navbar changes

--- ghi/src/HomePage.js
+++ ghi/src/HomePage.js
@@ -1,11 +1,112 @@
-import React, { useContext } from "react";
+// import React, { useContext } from "react";
+// import { AuthContext } from "@galvanize-inc/jwtdown-for-react";
+
+// const HomePage = () => {
+//   const authContext = useContext(AuthContext);
+
+//   if (!authContext.token) {
+//     return <p>You must be logged in to view this page.</p>;
+//   }
+
+//   return (
+//     <div>
+//       <h1>Welcome to the Home Page</h1>
+//       <p>This is the home page of our application.</p>
+//     </div>
+//   );
+// };
+
+// export default HomePage;
+
+// import React, { useContext, useState } from "react";
+// import { AuthContext } from "@galvanize-inc/jwtdown-for-react";
+// import { Modal, Button } from "react-bootstrap";
+// import LoginForm from "./LoginForm";
+// import SignUpForm from "./SignUpForm";
+
+// const HomePage = () => {
+//   const authContext = useContext(AuthContext);
+//   const [showModal, setShowModal] = useState(false);
+
+//   const handleCloseModal = () => {
+//     setShowModal(false);
+//   };
+
+//   const handleShowModal = () => {
+//     setShowModal(true);
+//   };
+
+//   if (!authContext.token) {
+//     return (
+//       <div>
+//         <p>You must be logged in to view this page.</p>
+//         <Button variant="primary" onClick={handleShowModal}>
+//           Login/Signup
+//         </Button>
+//         <Modal show={showModal} onHide={handleCloseModal}>
+//           <Modal.Header closeButton>
+//             <Modal.Title>Login/Signup</Modal.Title>
+//           </Modal.Header>
+//           <Modal.Body>
+//             <LoginForm />
+//             <SignUpForm />
+//           </Modal.Body>
+//         </Modal>
+//       </div>
+//     );
+//   }
+
+//   return (
+//     <div>
+//       <h1>Welcome to the Home Page</h1>
+//       <p>This is the home page of our application.</p>
+//     </div>
+//   );
+// };
+
+// export default HomePage;
+
+import React, { useContext, useState, useEffect } from "react";
 import { AuthContext } from "@galvanize-inc/jwtdown-for-react";
+import { Modal } from "react-bootstrap";
+import LoginForm from "./LoginForm";
+import SignUpForm from "./SignUpForm";
 
 const HomePage = () => {
   const authContext = useContext(AuthContext);
+  const [showModal, setShowModal] = useState(false);
+
+  useEffect(() => {
+    const delay = setTimeout(() => {
+      if (!authContext.token) {
+        setShowModal(true);
+      }
+    }, 5000);
+
+    return () => clearTimeout(delay);
+  }, [authContext.token]);
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
 
   if (!authContext.token) {
-    return <p>You must be logged in to view this page.</p>;
+    return (
+      <div>
+        <h1>Welcome to the Home Page</h1>
+        <p>This is the home page of our application.</p>
+        <Modal show={showModal} onHide={handleCloseModal}>
+          <Modal.Header closeButton>
+            <Modal.Title>Login/Signup</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <LoginForm />
+            <p>or</p>
+            <SignUpForm />
+          </Modal.Body>
+        </Modal>
+      </div>
+    );
   }
 
   return (
--- ghi/src/Nav.js
+++ ghi/src/Nav.js
@@ -1,42 +1,43 @@
 import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-success">
+    <nav className="navbar navbar-expand-lg navbar-dark">
       <div className="container-fluid">
-        <NavLink className="navbar-brand" to="/">
+        {/* <NavLink className="navbar-brand" to="/">
           Home
+        </NavLink> */}
+        <NavLink className="navbar-brand" to="/">
+          <img src="Logo_no_bg.png" alt="logo" className="logo-image" />
+        </NavLink>
+        {/* </div> */}
+        {/* <ul> */}
+        {/* <div className="nav-links"> */}
+        <NavLink to="/account/signup" activeclassname="active-link">
+          Signup
+        </NavLink>
+        <NavLink to="/Token" activeclassname="active-link">
+          Login
+        </NavLink>
+
+        <NavLink to="/locations/" activeclassname="active-link">
+          Locations List
+        </NavLink>
+        <NavLink to="/locations/form/" activeclassname="active-link">
+          Locations Form
+        </NavLink>
+        <NavLink to="/accessibility/form/" activeclassname="active-link">
+          Add an Accessibility
+        </NavLink>
+        <NavLink to="/review/form/" activeclassname="active-link">
+          Create a Review
+        </NavLink>
+        <NavLink to="/comments/new/" activeclassname="active-link">
+          Add a Comment
         </NavLink>
       </div>
-      <ul>
-        <div className="nav-links">
-          <NavLink to="/account/signup" activeclassname="active-link">
-            Signup
-          </NavLink>
-          <NavLink to="/Token" activeclassname="active-link">
-            Login
-          </NavLink>
-          <NavLink to="/account/signup" activeclassname="active-link">
-            Signup
-          </NavLink>
-          <NavLink to="/locations/" activeclassname="active-link">
-            Locations List
-          </NavLink>
-          <NavLink to="/locations/form/" activeclassname="active-link">
-            Locations Form
-          </NavLink>
-          <NavLink to="/accessibility/form/" activeclassname="active-link">
-            Add an Accessibility
-          </NavLink>
-          <NavLink to="/review/form/" activeclassname="active-link">
-            Create a Review
-          </NavLink>
-          <NavLink to="/comments/new/" activeclassname="active-link">
-            Add a Comment
-          </NavLink>
-        </div>
-      </ul>
+      {/* </ul> */}
     </nav>
   );
 }
