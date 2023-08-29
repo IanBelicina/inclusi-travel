195f192a48f1e2ff1c64d6d4accb46f561a2edad
Drew Norman-Meadows <norma1dj@gmail.com>
2023-08-29 11:13:47 -0700

    completed my test

--- api/tests/test_reviews.py
+++ api/tests/test_reviews.py
@@ -13,14 +13,12 @@ def test_get_reviews():
     #Arrange 
     app.dependency_overrides[ReviewQueries] = EmptyReviewQueries
 
-
     #ACT
     response = client.get("/api/reviews")
-
     app.dependency_overrides = {}
-
 
     #ASSERT
     #my get_reviews router throws a 404 error if there are no reviews
     assert response.status_code == 404
     assert response.json() == {'detail': 'No reviews found'}
+
