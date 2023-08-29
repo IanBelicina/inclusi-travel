9313230bf8fc280c700a4f21ee959a8f3134a94b
Drew Norman-Meadows <norma1dj@gmail.com>
2023-08-29 12:06:57 -0700

    formatted the file

--- api/tests/test_reviews.py
+++ api/tests/test_reviews.py
@@ -4,21 +4,21 @@ from queries.reviews import ReviewQueries
 
 client = TestClient(app)
 
+
 class EmptyReviewQueries:
     def get_all_reviews(self):
         return []
 
 
 def test_get_reviews():
-    #Arrange 
+    # Arrange
     app.dependency_overrides[ReviewQueries] = EmptyReviewQueries
 
-    #ACT
+    # ACT
     response = client.get("/api/reviews")
     app.dependency_overrides = {}
 
-    #ASSERT
-    #my get_reviews router throws a 404 error if there are no reviews
+    # ASSERT
+    # my get_reviews router throws a 404 error if there are no reviews
     assert response.status_code == 404
-    assert response.json() == {'detail': 'No reviews found'}
-
+    assert response.json() == {"detail": "No reviews found"}
