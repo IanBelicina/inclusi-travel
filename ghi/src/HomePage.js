ab5639b1fbb6a34e692f9e07734b6ed049ffd7a9
Drew Norman-Meadows <norma1dj@gmail.com>
2023-08-29 17:57:28 -0700

    new changes to homepage

--- ghi/src/HomePage.js
+++ ghi/src/HomePage.js
@@ -1,80 +1,13 @@
-// import React, { useContext } from "react";
-// import { AuthContext } from "@galvanize-inc/jwtdown-for-react";
-
-// const HomePage = () => {
-//   const authContext = useContext(AuthContext);
-
-//   if (!authContext.token) {
-//     return <p>You must be logged in to view this page.</p>;
-//   }
-
-//   return (
-//     <div>
-//       <h1>Welcome to the Home Page</h1>
-//       <p>This is the home page of our application.</p>
-//     </div>
-//   );
-// };
-
-// export default HomePage;
-
-// import React, { useContext, useState } from "react";
-// import { AuthContext } from "@galvanize-inc/jwtdown-for-react";
-// import { Modal, Button } from "react-bootstrap";
-// import LoginForm from "./LoginForm";
-// import SignUpForm from "./SignUpForm";
-
-// const HomePage = () => {
-//   const authContext = useContext(AuthContext);
-//   const [showModal, setShowModal] = useState(false);
-
-//   const handleCloseModal = () => {
-//     setShowModal(false);
-//   };
-
-//   const handleShowModal = () => {
-//     setShowModal(true);
-//   };
-
-//   if (!authContext.token) {
-//     return (
-//       <div>
-//         <p>You must be logged in to view this page.</p>
-//         <Button variant="primary" onClick={handleShowModal}>
-//           Login/Signup
-//         </Button>
-//         <Modal show={showModal} onHide={handleCloseModal}>
-//           <Modal.Header closeButton>
-//             <Modal.Title>Login/Signup</Modal.Title>
-//           </Modal.Header>
-//           <Modal.Body>
-//             <LoginForm />
-//             <SignUpForm />
-//           </Modal.Body>
-//         </Modal>
-//       </div>
-//     );
-//   }
-
-//   return (
-//     <div>
-//       <h1>Welcome to the Home Page</h1>
-//       <p>This is the home page of our application.</p>
-//     </div>
-//   );
-// };
-
-// export default HomePage;
-
 import React, { useContext, useState, useEffect } from "react";
 import { AuthContext } from "@galvanize-inc/jwtdown-for-react";
-import { Modal } from "react-bootstrap";
+import { Modal, Button } from "react-bootstrap";
 import LoginForm from "./LoginForm";
 import SignUpForm from "./SignUpForm";
 
 const HomePage = () => {
   const authContext = useContext(AuthContext);
   const [showModal, setShowModal] = useState(false);
+  const [showSignUpForm, setShowSignUpForm] = useState(false);
 
   useEffect(() => {
     const delay = setTimeout(() => {
@@ -88,21 +21,36 @@ const HomePage = () => {
 
   const handleCloseModal = () => {
     setShowModal(false);
+    setShowSignUpForm(false);
+  };
+
+  const handleShowSignUpForm = () => {
+    setShowSignUpForm(true);
   };
 
   if (!authContext.token) {
     return (
       <div>
-        <h1>Travel With Confidence</h1>
+        <h1 className="travel-with-confidence">Travel With Confidence</h1>
         <p></p>
-        <Modal show={showModal} onHide={handleCloseModal}>
-          <Modal.Header closeButton>
+        <Modal
+          show={showModal}
+          onHide={handleCloseModal}
+          className="customModal"
+        >
+          {/* <Modal.Header>
             <Modal.Title>Login/Signup</Modal.Title>
-          </Modal.Header>
+          </Modal.Header> */}
           <Modal.Body>
             <LoginForm />
-            <p>or</p>
-            <SignUpForm />
+            <div>
+              {!showSignUpForm && (
+                <Button variant="primary" onClick={handleShowSignUpForm}>
+                  Register Here
+                </Button>
+              )}
+              {showSignUpForm && <SignUpForm />}
+            </div>
           </Modal.Body>
         </Modal>
       </div>
@@ -112,7 +60,6 @@ const HomePage = () => {
   return (
     <div>
       <h1 className="travel-with-confidence">Travel With Confidence</h1>
-      <p>This is the home page of our application.</p>
     </div>
   );
 };
