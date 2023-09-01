290eab87e444ad14d734049c5d31655e64330368
Joey <li.joey.h@gmail.com>
2023-08-31 17:27:10 -0700

    fixed formatting

--- api/tests/test_locations.py
+++ api/tests/test_locations.py
@@ -9,10 +9,11 @@ class EmptyLocationQueries:
     def get_all_locations(self):
         return []
 
+
 def test_get_locations():
     app.dependency_overrides[LocationQueries] = EmptyLocationQueries
     response = client.get("/api/locations")
     app.dependency_overrides = {}
 
     assert response.status_code == 200
-    assert response.json() == {"locations":[]}
+    assert response.json() == {"locations": []}
