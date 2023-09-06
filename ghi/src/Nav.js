3381b3c5cd4b782117d7ad8a38866b8bfa79fa8a
Drew Norman-Meadows <norma1dj@gmail.com>
2023-09-06 12:06:51 -0700

    get right with main

--- ghi/src/Nav.js
+++ ghi/src/Nav.js
@@ -10,11 +10,7 @@ function Nav() {
     <nav className="navbar navbar-expand-lg navbar-dark">
       <div className="container-fluid">
         <NavLink className="navbar-brand" to="/">
-<<<<<<< HEAD
-          <img src="/Logo_no_bg.png" alt="logo" className="logo-image" />
-=======
           <img src={require("./Logo_no_bg.png")} alt="" />
->>>>>>> main
         </NavLink>
         <button
           className="navbar-toggler"
